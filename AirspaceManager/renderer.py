80262a0663d27a82857e844970afbb3d4f28cf34
janzahradka <jan.zahradka@gmail.com>
2025-03-01 10:41:55 +0100

    vykreslování do mapy

--- AirspaceManager/renderer.py
+++ AirspaceManager/renderer.py
@@ -2,8 +2,12 @@ import folium
 import webbrowser
 import numpy as np
 from extractor.convertor import Convertor
+from folium import MacroElement
+from jinja2 import Template
+from shapely.geometry import Polygon
+from shapely.ops import transform
+import pyproj
 
-# Definice funkcí na úrovni modulu pro styl a zvýraznění
 def polygon_style_function(feature):
     return {
         'fillColor': 'green',
@@ -20,6 +24,37 @@ def constant_highlight(feature):
         'weight': 3
     }
 
+class HoverScript(MacroElement):
+    _template = Template(u"""
+        {% macro script(this, kwargs) %}
+        function onEachFeature(feature, layer) {
+            layer.on('mouseover', function(e) {
+                document.getElementById('infoBox').innerHTML = feature.properties.popup;
+                layer.setStyle({
+                    fillColor: 'yellow',
+                    color: 'yellow',
+                    fillOpacity: 0.7,
+                    weight: 3
+                });
+            });
+            layer.on('mouseout', function(e) {
+                document.getElementById('infoBox').innerHTML = "Hover over an airspace...";
+                layer.setStyle({
+                    fillColor: 'green',
+                    color: 'green',
+                    fillOpacity: 0.4,
+                    weight: 2
+                });
+            });
+        }
+        {{this._parent.get_name()}}.eachLayer(function(layer) {
+            if(layer.feature){
+                onEachFeature(layer.feature, layer);
+            }
+        });
+        {% endmacro %}
+    """)
+
 class Renderer:
     """Renderer for drawing airspaces on a map using Folium"""
 
@@ -27,11 +62,6 @@ class Renderer:
         self.airspaces = airspaces  # List of Airspace objects
 
     def get_coordinate(self, coord_input):
-        """
-        If coord_input is a dict, return it.
-        If it is a string, parse it using Convertor.extract_coodinate_from_text.
-        Otherwise, return None.
-        """
         if isinstance(coord_input, dict):
             return coord_input
         elif isinstance(coord_input, str):
@@ -40,14 +70,8 @@ class Renderer:
             return None
 
     def calculate_arc_points(self, center, start, end, direction="+", num_points=100):
-        """
-        Calculates arc points between start and end around center.
-        direction: '+' for clockwise, '-' for counterclockwise.
-        num_points: number of points on the arc.
-        """
         def to_rad(deg):
             return deg * np.pi / 180
-
         def to_deg(rad):
             return rad * 180 / np.pi
 
@@ -84,15 +108,74 @@ class Renderer:
             arc_points.append((to_deg(lat), to_deg(lon)))
         return arc_points
 
-    def render_map(self):
-        """Creates a map and draws all airspaces."""
-        map_object = folium.Map(location=[50.0, 15.0], zoom_start=8)
-        all_coordinates = []
+    @staticmethod
+    def compute_polygon_area(polygon_points):
+        if len(polygon_points) < 4:
+            return 0
+        coords = [(lon, lat) for (lat, lon) in polygon_points]
+        poly = Polygon(coords)
+        transformer = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True)
+        poly_m = transform(transformer.transform, poly)
+        return poly_m.area
 
+    def approximate_circle_polygon(self, center, radius_m, num_points=50):
+        lat, lon = center
+        points = []
+        for i in range(num_points):
+            angle = 2 * np.pi * i / num_points
+            dlat = (radius_m / 111320) * np.cos(angle)
+            dlon = (radius_m / (111320 * np.cos(np.radians(lat)))) * np.sin(angle)
+            points.append((lat + dlat, lon + dlon))
+        points.append(points[0])
+        return points
+
+    def render_map(self):
+        map_object = folium.Map(location=[50.0, 15.0], zoom_start=8)
+        # Přidáme do HTML div pro infoBox
+        info_html = """
+        <div id="infoBox" style="position: fixed; top: 10px; right: 10px; width: 300px;
+             padding: 10px; background-color: white; border: 1px solid gray;
+             z-index: 1000; font-size: 14px;">
+          <b>Airspace Details</b><br>
+          Hover over an airspace...
+        </div>
+        """
+        map_object.get_root().html.add_child(folium.Element(info_html))
+
+        all_coordinates = []
+        airspaces_with_area = []
         for airspace in self.airspaces:
+            polygon_points = []
+            found_circle = False
+            circle_area = 0
+            for command in airspace.draw_commands:
+                if command["type"] == "polygon_point":
+                    coord = self.get_coordinate(command["polygon_point_coordinate"])
+                    if coord:
+                        polygon_points.append((coord["lat"], coord["lon"]))
+                elif command["type"] == "circle":
+                    found_circle = True
+                    radius_nm = float(command["circle_radius"])
+                    radius_m = radius_nm * 1852
+                    circle_area = np.pi * (radius_m ** 2)
+            if polygon_points:
+                if polygon_points[0] != polygon_points[-1]:
+                    polygon_points.append(polygon_points[0])
+                area = Renderer.compute_polygon_area(polygon_points)
+            elif found_circle:
+                area = circle_area
+            else:
+                area = 0
+            airspaces_with_area.append((airspace, area))
+        airspaces_with_area.sort(key=lambda x: x[1], reverse=True)
+        print("Computed order of airspaces (Order, Area, Airspace):")
+        for idx, (airspace, area) in enumerate(airspaces_with_area, start=1):
+            print(f"{idx}. Area: {area:.2f}\n{airspace}\n")
+        for airspace, area in airspaces_with_area:
+            popup_content = self.build_popup_content(airspace)
             draw_commands = airspace.draw_commands
             polygon_points = []
-
+            has_polygon = False
             for command in draw_commands:
                 if command["type"] == "polygon_point":
                     coord = self.get_coordinate(command["polygon_point_coordinate"])
@@ -102,37 +185,28 @@ class Renderer:
                     lon = coord["lon"]
                     polygon_points.append((lat, lon))
                     all_coordinates.append((lat, lon))
-
+                    has_polygon = True
                 elif command["type"] == "circle":
-                    self.render_circle(map_object, command, all_coordinates)
-
+                    self.render_circle(map_object, command, all_coordinates, popup_content)
                 elif command["type"] == "arc":
                     self.render_arc(map_object, command, polygon_points, all_coordinates)
-
-            if polygon_points:
-                polygon_points.append(polygon_points[0])  # Close the polygon
-                # Convert coordinates to GeoJSON format ([lon, lat])
+            if has_polygon:
+                polygon_points.append(polygon_points[0])
                 geo_coords = [[lon, lat] for (lat, lon) in polygon_points]
-                popup_content = self.build_popup_content(airspace)
                 polygon_geojson = {
                     "type": "Feature",
-                    "properties": {
-                        "popup": popup_content
-                    },
-                    "geometry": {
-                        "type": "Polygon",
-                        "coordinates": [geo_coords]
-                    }
+                    "properties": {"popup": popup_content},
+                    "geometry": {"type": "Polygon", "coordinates": [geo_coords]}
                 }
-                # Vytvoříme GeoJsonPopup, který zobrazuje vlastnost "popup"
                 popup = folium.GeoJsonPopup(fields=["popup"], labels=False)
-                folium.GeoJson(
+                geojson = folium.GeoJson(
                     polygon_geojson,
                     style_function=polygon_style_function,
                     highlight_function=constant_highlight,
                     popup=popup,
-                ).add_to(map_object)
-
+                )
+                geojson.add_child(HoverScript())
+                geojson.add_to(map_object)
         if all_coordinates:
             lats = [coord[0] for coord in all_coordinates]
             lons = [coord[1] for coord in all_coordinates]
@@ -140,23 +214,18 @@ class Renderer:
             south = min(lats)
             east = max(lons)
             west = min(lons)
-
             margin_factor = 1.0
             lat_margin = (north - south) * margin_factor
             lon_margin = (east - west) * margin_factor
-
             north += lat_margin
             south -= lat_margin
             east += lon_margin
             west -= lon_margin
-
             map_object.fit_bounds([(south, west), (north, east)])
-
         map_object.save("airspace_map.html")
         webbrowser.open("airspace_map.html")
 
-    def render_circle(self, map_object, command, all_coordinates):
-        """Renders a circle and adds its bounding box to all_coordinates."""
+    def render_circle(self, map_object, command, all_coordinates, popup_content=None):
         coord = self.get_coordinate(command["circle_center_coordinate"])
         if coord is None:
             return
@@ -164,24 +233,26 @@ class Renderer:
         lon = coord["lon"]
         radius_nm = float(command["circle_radius"])
         radius_m = radius_nm * 1852
-
-        folium.Circle(
-            location=(lat, lon),
-            radius=radius_m,
-            color='blue',
-            fill=True,
-            fill_color='lightblue'
-        ).add_to(map_object)
-
-        dlat = radius_m / 111000
-        dlon = radius_m / (111000 * np.cos(np.radians(lat)))
-        all_coordinates.append((lat - dlat, lon - dlon))
-        all_coordinates.append((lat - dlat, lon + dlon))
-        all_coordinates.append((lat + dlat, lon - dlon))
-        all_coordinates.append((lat + dlat, lon + dlon))
+        circle_points = self.approximate_circle_polygon((lat, lon), radius_m, num_points=50)
+        for pt in circle_points:
+            all_coordinates.append(pt)
+        geo_coords = [[pt[1], pt[0]] for pt in circle_points]
+        polygon_geojson = {
+            "type": "Feature",
+            "properties": {"popup": popup_content if popup_content else ""},
+            "geometry": {"type": "Polygon", "coordinates": [geo_coords]}
+        }
+        popup = folium.GeoJsonPopup(fields=["popup"], labels=False)
+        geojson = folium.GeoJson(
+            polygon_geojson,
+            style_function=polygon_style_function,
+            highlight_function=constant_highlight,
+            popup=popup,
+        )
+        geojson.add_child(HoverScript())
+        geojson.add_to(map_object)
 
     def render_arc(self, map_object, command, polygon_points, all_coordinates):
-        """Renders an arc as part of a circle."""
         center = self.get_coordinate(command["arc_center_coordinate"])
         start = self.get_coordinate(command["arc_start_point_coordinate"])
         end = self.get_coordinate(command["arc_end_point_coordinate"])
@@ -191,18 +262,14 @@ class Renderer:
         start_coords = (start["lat"], start["lon"])
         end_coords = (end["lat"], end["lon"])
         direction = command["arc_direction"]
-
         arc_points = self.calculate_arc_points(center_coords, start_coords, end_coords, direction)
-
         if polygon_points:
             polygon_points.append(polygon_points[-1])
         polygon_points.extend(arc_points)
         polygon_points.append(end_coords)
-
         all_coordinates.extend(arc_points)
 
     def build_popup_content(self, airspace):
-        """Builds popup content with airspace details."""
         content = ""
         if airspace.name:
             content += f"Name: <b>{airspace.name}</b><br>"
